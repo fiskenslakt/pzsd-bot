d143da2e447c1555ba8f04b71499dcfbaea2f77d
Derek <sjukledighet@gmail.com>
2024-05-04 17:01:54 -0400

    Change the math to be cuter

--- pzsd_bot/__init__.py
+++ pzsd_bot/__init__.py
@@ -9,7 +9,7 @@ logger.setLevel(Bot.log_level)
 log_file = Path(__file__).parents[1] / Path("logs", "pzsd_bot.log")
 log_file.parent.mkdir(exist_ok=True)
 handler = handlers.RotatingFileHandler(
-    log_file, maxBytes=1024 * 1024 * 3, backupCount=2, encoding="utf8"
+    log_file, maxBytes=3 * 1024**2, backupCount=2, encoding="utf8"
 )
 handler.setFormatter(
     logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s")
