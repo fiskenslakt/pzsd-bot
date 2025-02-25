822b250c2f67f05ed27a7bf709d739575c8018a5
Derek <sjukledighet@gmail.com>
2025-02-25 02:42:35 -0500

    Add modal for editing triggers

--- pzsd_bot/ui/triggers/modals.py
+++ pzsd_bot/ui/triggers/modals.py
@@ -1,9 +1,10 @@
 import logging
 import re
+from typing import List, Tuple
 
 from discord import Bot, InputTextStyle, Interaction
 from discord.ui import InputText, Modal
-from sqlalchemy import insert
+from sqlalchemy import delete, func, insert, update
 
 from pzsd_bot.db import Session
 from pzsd_bot.model import trigger_group, trigger_pattern, trigger_response
@@ -11,16 +12,40 @@ from pzsd_bot.model import trigger_group, trigger_pattern, trigger_response
 logger = logging.getLogger(__name__)
 
 
-def is_valid_regex(pattern: str) -> bool:
-    try:
-        re.compile(pattern)
-    except re.error:
-        return False
-    else:
-        return True
+class TriggerModalMixin:
+    @staticmethod
+    def is_valid_regex(pattern: str) -> bool:
+        try:
+            re.compile(pattern)
+        except re.error:
+            return False
+        else:
+            return True
+
+    async def get_input(
+        self, interaction: Interaction
+    ) -> Tuple[List[str], List[str]] | None:
+        if self.is_regex:
+            pattern = self.children[0].value
+            if pattern and self.is_valid_regex(pattern):
+                patterns = [pattern]
+            else:
+                logger.info(
+                    "%s submitted trigger with invalid regex, doing nothing.",
+                    interaction.user.name,
+                )
+                await interaction.respond("Invalid regex, failed to add trigger.")
+                return None
+        else:
+            patterns = self.children[0].value.lower().split(",")
+
+        # TODO confirm if this is how i want to parse responses
+        responses = self.children[1].value.splitlines()
+
+        return patterns, responses
 
 
-class AddTriggerModal(Modal):
+class AddTriggerModal(Modal, TriggerModalMixin):
     def __init__(self, *args, is_regex: bool, bot: Bot, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
@@ -32,36 +57,14 @@ class AddTriggerModal(Modal):
         self.add_item(InputText(label=pattern_label, style=InputTextStyle.long))
         self.add_item(InputText(label="Response(s)", style=InputTextStyle.long))
 
-    async def callback(self, interaction: Interaction):
-        logger.debug(
-            "Performing AddTriggerModal callback for user with id=%s",
-            interaction.user.id,
-        )
-
-        if self.is_regex:
-            pattern = self.children[0].value
-            if pattern and is_valid_regex(pattern):
-                patterns = [pattern]
-            else:
-                logger.info(
-                    "%s submitted trigger with invalid regex, doing nothing.",
-                    interaction.user.name,
-                )
-                await interaction.respond("Invalid regex, failed to add trigger.")
-                return
-        else:
-            patterns = self.children[0].value.lower().split(",")
-
-        # TODO confirm if this is how i want to parse responses
-        responses = self.children[1].value.splitlines()
-
+    async def add_trigger_to_db(
+        self, owner: int, patterns: List[str], responses: List[str]
+    ) -> int:
         logger.info("Adding new trigger to db")
 
         async with Session.begin() as session:
             result = await session.execute(
-                insert(trigger_group)
-                .values(owner=interaction.user.id)
-                .returning(trigger_group.c.id)
+                insert(trigger_group).values(owner=owner).returning(trigger_group.c.id)
             )
             group_id: int = result.scalar_one()
 
@@ -88,6 +91,23 @@ class AddTriggerModal(Modal):
             )
         logger.info("Added trigger to db with group_id=%s", group_id)
 
+        return group_id
+
+    async def callback(self, interaction: Interaction):
+        logger.debug(
+            "Performing AddTriggerModal callback for user with id=%s",
+            interaction.user.id,
+        )
+
+        modal_input = await self.get_input(interaction)
+        if modal_input is None:
+            return
+
+        patterns, responses = modal_input
+        group_id = await self.add_trigger_to_db(
+            interaction.user.id, patterns, responses
+        )
+
         # send on_trigger_added event
         # to add trigger into memory
         self.bot.dispatch(
@@ -99,3 +119,112 @@ class AddTriggerModal(Modal):
         )
 
         await interaction.respond("Successfully added trigger", ephemeral=True)
+
+
+class EditTriggerModal(Modal, TriggerModalMixin):
+    def __init__(
+        self,
+        *args,
+        patterns: List[str],
+        responses: List[str],
+        is_regex: bool,
+        group_id: int,
+        bot: Bot,
+        **kwargs,
+    ) -> None:
+        super().__init__(*args, **kwargs)
+
+        self.bot = bot
+
+        self.is_regex = is_regex
+        pattern_label = "Trigger {}pattern".format("regex " if is_regex else "")
+
+        self.old_patterns = patterns
+        self.group_id = group_id
+
+        self.add_item(
+            InputText(
+                label=pattern_label,
+                style=InputTextStyle.long,
+                value=",".join(dict.fromkeys(patterns)),
+            )
+        )
+        self.add_item(
+            InputText(
+                label="Response(s)",
+                style=InputTextStyle.long,
+                value="\n".join(dict.fromkeys(responses)),
+            )
+        )
+
+    async def edit_trigger_in_db(
+        self, new_patterns: List[str], new_responses: List[str]
+    ) -> None:
+        logger.info("Modifying trigger in db with id=%s", self.group_id)
+
+        async with Session.begin() as session:
+            await session.execute(
+                delete(trigger_pattern).where(
+                    trigger_pattern.c.group_id == self.group_id
+                )
+            )
+            await session.execute(
+                delete(trigger_response).where(
+                    trigger_response.c.group_id == self.group_id
+                )
+            )
+
+            await session.execute(
+                insert(trigger_pattern),
+                [
+                    {
+                        "pattern": pattern,
+                        "group_id": self.group_id,
+                        "is_regex": self.is_regex,
+                    }
+                    for pattern in new_patterns
+                ],
+            )
+            await session.execute(
+                insert(trigger_response),
+                [
+                    {
+                        "response": response,
+                        "group_id": self.group_id,
+                    }
+                    for response in new_responses
+                ],
+            )
+
+            await session.execute(
+                update(trigger_group)
+                .where(trigger_group.c.id == self.group_id)
+                .values(updated_at=func.now())
+            )
+
+    async def callback(self, interaction: Interaction):
+        logger.debug(
+            "Performing EditTriggerModal callback for user with id=%s",
+            interaction.user.id,
+        )
+
+        modal_input = await self.get_input(interaction)
+        if modal_input is None:
+            return
+
+        patterns, responses = modal_input
+
+        await self.edit_trigger_in_db(patterns, responses)
+
+        # send on_trigger_added event
+        # to add trigger into memory
+        self.bot.dispatch(
+            "trigger_modified",
+            old_patterns=self.old_patterns,
+            new_patterns=patterns,
+            new_responses=responses,
+            is_regex=self.is_regex,
+            group_id=self.group_id,
+        )
+
+        await interaction.respond("Successfully edited trigger", ephemeral=True)
